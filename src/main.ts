73cb45b8d95722fa00d3264bb5848bfa1f39d1d9
Emmanuella <emmanuella.chukwu77@gmail.com>
2025-01-16 13:40:17 +0100

    fix cors header

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ async function bootstrap() {
       // production frontend URL here
     ],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
   });
   await app.listen(process.env.PORT ?? 4006);
