17c74484317a5504a451eefd8fffbb7e3e352004
Emmanuella <emmanuella.chukwu77@gmail.com>
2025-01-17 13:55:47 +0100

    more cors configuration

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,24 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use((req, res, next) => {
+    if (req.method === 'OPTIONS') {
+      res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+      res.header(
+        'Access-Control-Allow-Methods',
+        'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+      );
+      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+      return res.sendStatus(200);
+    }
+    next();
+  });
+
   app.enableCors({
     origin: 'http://localhost:3000',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
-    exposedHeaders: ['Access-Control-Allow-Origin'],
-    credentials: false,
   });
-  await app.listen(process.env.PORT ?? 4006);
+  await app.listen(process.env.PORT || 4006);
 }
 bootstrap();
