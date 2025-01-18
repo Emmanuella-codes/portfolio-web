d40a984fc9317c6625531e972708493f7a9d9cf7
Emmanuella <emmanuella.chukwu77@gmail.com>
2025-01-18 02:16:16 +0100

    test cors with deployed frontend url

--- src/main.ts
+++ src/main.ts
@@ -3,21 +3,21 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use((req, res, next) => {
-    if (req.method === 'OPTIONS') {
-      res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-      res.header(
-        'Access-Control-Allow-Methods',
-        'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-      );
-      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-      return res.sendStatus(200);
-    }
-    next();
-  });
+  // app.use((req, res, next) => {
+  //   if (req.method === 'OPTIONS') {
+  //     res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  //     res.header(
+  //       'Access-Control-Allow-Methods',
+  //       'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  //     );
+  //     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  //     return res.sendStatus(200);
+  //   }
+  //   next();
+  // });
 
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000', 'https://ella-portfolio-v2.vercel.app/'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   });
