0da35c1cb91407381581e664009480d0a2636700
Emmanuella <emmanuella.chukwu77@gmail.com>
2025-01-24 22:11:49 +0100

    remove trailing slash in cors config

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://ella-portfolio-v2.vercel.app/'],
+    origin: ['http://localhost:3000', 'https://ella-portfolio-v2.vercel.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   });
